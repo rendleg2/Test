1005f9a7651ff852c650551a9131f31c4e98a190
rendleg2 <20136@rosmini.school.nz>
2024-04-10 10:51:43 +1200

    More Tidyups and added hover afects to navbar

--- NewServer/java.js
+++ NewServer/java.js
@@ -1,18 +1,9 @@
-/* global bootstrap: false */
-(() => {
-  'use strict'
-  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(tooltipTriggerEl => {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
 
-
+// Controlling the Iframes link
 function loadClassYearIframe(classYearCode,classYearTitle)
 {
   var classUrl = "http://student.share.johnlearnings.com/Rosmini/" + classYearCode + "/";
   document.getElementById("yearHeader").innerHTML = classYearTitle;
   document.getElementById("yearPopout").href = classUrl;
-  // alert(classUrl);
   document.getElementById("classiframe").src = classUrl;
 }
